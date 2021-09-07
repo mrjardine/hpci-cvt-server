cc636bae314181fb5ebb67dbea1817fdc4865c68
Mike Jardine <jardinemr@gmail.com>
2021-09-07 13:01:12 -0300

    added dateRE to notification route date param

--- routes/notifications.js
+++ routes/notifications.js
@@ -7,16 +7,18 @@ const notificationRoutes = (app) => {
   const notificationsPathPrefix = apiPathPrefix.concat('notifications');
   const langRE = '(en|fr)';
   const tokenRE = '(\\b\\S{22}\\b)'; // i.e. 22 length, non-whitespace id between [] in ExponentPushToken[...]
-  // TODO: dateRE (fmt: yyyy-mm-dd, valid, optional), retrieveBookmarks
+  const dateRE = '(\\d{4}-\\d{2}-\\d{2})';
 
-  // /notifications/:token/:language/:date?
+  // TODO: process request (e.g. token exists, date is valid and within last x days, retrieveBookmarks...)
+
+  // /notifications/:token/:language/:date
   app
     .route(
       notificationsPathPrefix
         .concat('/:token')
         .concat(tokenRE)
         .concat('/:language'.concat(langRE))
-        .concat('/:date?')
+        .concat('/:date'.concat(dateRE))
     )
     .get([retrieveNotifications], (req, res) => {
       res.send();
