8b34f1f6dd8cca93c7cdae075e4f7004c8c27b9d
Mike Jardine <jardinemr@gmail.com>
2022-05-13 18:46:05 -0300

    added endpoints to get push results

--- routes/push.js
+++ routes/push.js
@@ -1,12 +1,18 @@
 const { apiPathPrefix } = require('../config');
 const { prepareMessages } = require('../notifications/message');
 const { sendMessages, readReceipts } = require('../notifications/expo');
-const { addNotifications } = require('../data/notification');
+const {
+  addNotifications,
+  getPushResults,
+  getPushResultsForNotification
+} = require('../data/notification');
 const { addTickets, getTickets, removeTickets } = require('../data/ticket');
 const { addReceipts } = require('../data/receipt');
 
 const pushRoutes = (app) => {
   const pushPathPrefix = apiPathPrefix.concat('push');
+  const uuidRE =
+    '([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})';
 
   // /push/send
   app
@@ -29,6 +35,21 @@ const pushRoutes = (app) => {
         res.send();
       }
     );
+
+  // /push/results
+  app
+    .route(pushPathPrefix.concat('/results'))
+    .get([getPushResults], (req, res) => {
+      // console.log("get push results...", req.route.path);
+      res.send();
+    });
+
+  // /push/results/:notificationId
+  app
+    .route(pushPathPrefix.concat('/results/:notificationId').concat(uuidRE))
+    .get([getPushResultsForNotification], (req, res) => {
+      res.send();
+    });
 };
 
 module.exports = pushRoutes;
